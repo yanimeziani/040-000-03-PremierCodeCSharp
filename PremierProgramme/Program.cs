4b124a2d5fc9141b7a03e5847f090f1f7d04fd48
Pierre-François Léon <pierrefrancois.leon@gmail.com>
2022-01-16 22:21:35 -0500

    Mise en place du programme de base

---
+++ PremierProgramme/Program.cs
@@ -0,0 +1,5 @@
+﻿// Exécution du programme à partir de la ligne de commande
+// dotnet run --project PremierProgramme
+
+// L'exécution de Console.Out.WriteLine(<valeur>); affiche une valeur sur la sortie standard.
+Console.Out.WriteLine();
---
+++ tests/executer.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+export DOTNET_CLI_HOME=/home/runner
+dotnet run --project ../PremierProgramme
