5ba31d6643811abc9e7bc3f25c3deac55896e598
Yani Meziani <yani.meziani@protonmail.com>
2023-01-16 14:02:09 -0500

    Changement du texte, à la

--- PremierProgramme/Program.cs
+++ PremierProgramme/Program.cs
@@ -1,2 +1,2 @@
 ﻿// L'exécution de Console.Out.WriteLine(<valeur>); affiche une valeur sur la sortie standard.
-Console.Out.WriteLine("Bienvenue en formation continue");
+Console.Out.WriteLine("Bienvenue à la formation continue");
